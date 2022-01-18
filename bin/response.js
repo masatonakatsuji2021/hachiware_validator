fec8b4c846dfd7e90a4d17f1957e00395ca031f5
nakajima-satoru <nakajima@email.com>
2022-01-18 21:28:53 +0900

    1/18

--- bin/response.js
+++ bin/response.js
@@ -1,3 +1,17 @@
+/**
+ * =========================================================================================
+ * hachiware_validator
+ * 
+ * nput validation for Node.js.
+ * 
+ * License : MIT License. 
+ * Since   : 2022.01.08
+ * Author  : Nakatsuji Masato 
+ * GitHub  : https://github.com/masatonakatsuji2021/hachiware_validator
+ * npm     : https://www.npmjs.com/package/hachiware_validator
+ * =========================================================================================
+ */
+
 const validatorResponse = function(validates){
 
     var colums = Object.keys(validates);
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+/**
+ * =========================================================================================
+ * hachiware_validator
+ * 
+ * nput validation for Node.js.
+ * 
+ * License : MIT License. 
+ * Since   : 2022.01.08
+ * Author  : Nakatsuji Masato 
+ * GitHub  : https://github.com/masatonakatsuji2021/hachiware_validator
+ * npm     : https://www.npmjs.com/package/hachiware_validator
+ * =========================================================================================
+ */
+
 module.exports = require("./bin/validator.js");
